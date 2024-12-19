a89d2b72d418d4cd087ef152d7a703ee834fcc34
Ramana <ther6014@gmail.com>
2024-12-20 05:27:43 +0530

    DB commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "sundaramg@1612",
-  database: "FORMVALIDATE",
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "sundaramg@1612",
+  database: process.env.DB_NAME || "FORMVALIDATE" ,
+  port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
